0fb13d13e12f5bb8ec9d53653bbc1a59e1a5dd3b
Dave Augustus <davetweetlive@gmail.com>
2023-12-22 23:13:46 +0530

    Added LL as CLI
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- lesson019/main.go
+++ lesson019/main.go
@@ -61,7 +61,7 @@ func (l *LinkedList) Display() {
 	current := l.Head
 
 	for current != nil {
-		fmt.Printf("%d and %v, \n", current.Data, current.Next)
+		fmt.Printf("%d, \t", current.Data)
 		current = current.Next
 	}
 
@@ -115,15 +115,63 @@ func (l *LinkedList) DeleteFromSpecifiedLocation(location int) {
 }
 
 func main() {
+	var choice int
+	var nodeValue int
+	var nodeLocation int
 	list := &LinkedList{}
 
-	list.InsertEnd(1)
-	list.InsertEnd(2)
-	list.InsertEnd(3)
-	list.InsertAtBeginning(5)
-	list.InsertNodeAtTheSpecifiedLocation(9, 3)
-	list.DeleteFirstNode()
-	list.DeleteFromSpecifiedLocation(3)
-	list.Display()
+	for {
+		fmt.Println("\nEnter your choice!")
+		fmt.Println("_______________________________________________________________")
+		fmt.Printf("1. Insert at the beginning\t2. Insert at the end\n")
+		fmt.Printf("3. Insert at the specific location\t 4. Display")
+		fmt.Println()
+		fmt.Printf("5. Delete the first node\t6. Delete the last node\n")
+		fmt.Printf("7. Delete specific node \t8. Press -1 to Exit\n")
+
+		fmt.Scanf("%d", &choice)
+
+		switch choice {
+		case -1:
+			return
+		case 1:
+			fmt.Println("Enter a node value: ")
+			fmt.Scanf("%d", &nodeValue)
+			list.InsertAtBeginning(nodeValue)
+		case 2:
+			fmt.Println("Enter a node value: ")
+			fmt.Scanf("%d", &nodeValue)
+			list.InsertEnd(nodeValue)
+		case 3:
+			fmt.Println("Enter a node value: ")
+			fmt.Scanf("%d", &nodeValue)
+			fmt.Println("Enter a node location: ")
+			fmt.Scanf("%d", &nodeLocation)
+			list.InsertNodeAtTheSpecifiedLocation(nodeValue, nodeLocation)
+		case 4:
+			list.Display()
+		case 5:
+			list.DeleteFirstNode()
+		case 6:
+			list.DeleteLastNode()
+		case 7:
+			fmt.Println("Enter a node location: ")
+			fmt.Scanf("%d", &nodeLocation)
+			list.DeleteFromSpecifiedLocation(nodeLocation)
+		case 8:
+			return
+		default:
+			fmt.Println("Invalid input!")
+		}
+	}
+
+	// list.InsertEnd(1)
+	// list.InsertEnd(2)
+	// list.InsertEnd(3)
+	// list.InsertAtBeginning(5)
+	// list.InsertNodeAtTheSpecifiedLocation(9, 3)
+	// list.DeleteFirstNode()
+	// list.DeleteFromSpecifiedLocation(3)
+	// list.Display()
 
 }
